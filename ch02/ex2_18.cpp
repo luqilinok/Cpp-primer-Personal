804d921f4b5e44a2105681787101bf1aff2061c3
luqilinok <1214840350@qq.com>
2018-03-15 16:36:05 +0800

    Create ex2_18.cpp

---
+++ ch02/ex2_18.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main()
+{
+	int i = 5, j = 10;
+	int *p = &i;
+	std::cout << p << " " << *p << std::endl;
+	p = &j;
+	std::cout << p << " " << *p << std::endl;
+	*p = 20;
+	std::cout << p << " " << *p << std::endl;
+	j = 30;
+	std::cout << p << " " << *p << std::endl;
+	
+	system("pause");
+	return 0;
+}
