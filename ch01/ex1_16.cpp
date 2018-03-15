7d0247f61eded78c4818256f46e50d4a22a572be
luqilinok <1214840350@qq.com>
2018-03-15 15:19:59 +0800

    Create ex1_16.cpp

---
+++ ch01/ex1_16.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+	int sum = 0, value = 0;
+	std::cout << "请输入一些数，按Ctrl+Z结束" << std::endl;
+	for (; std::cin >> value;)
+		sum += value;
+	std::cout << "读入的数之和为" << sum<<std::endl;
+	system("pause");
+	return 0;
+}
