8cd1d0f2faad6456ca2e145b6ed1dec76a134c80
luqilinok <1214840350@qq.com>
2018-03-15 16:45:43 +0800

    Create eg2_1_3_3.cpp

---
+++ ch02/eg2_1_3_3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//int units_sold = { 0 };
+	//int units_sold{ 0 };
+	int units_sold(0);
+	std::cout << units_sold << std::endl;
+	system("pause");
+	return 0;
+}
