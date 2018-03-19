1e65adc187184cad47d5a05028e6127cb6d0efa6
luqilinok <1214840350@qq.com>
2018-03-19 14:11:47 +0800

    Create ex2_38.cpp

---
+++ ch02/ex2_38.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<typeinfo>
+
+int main()
+{
+	int a = 3;
+	auto c1 = a;
+	decltype(a) c2 = a;
+	decltype((a)) c3 = a;
+
+	const int d = 5;
+	auto f1 = d;
+	decltype(d) f2 = d;
+
+	std::cout << typeid(c1).name() << std::endl;
+	std::cout << typeid(c2).name()<< std::endl;
+	std::cout << typeid(c3).name()<< std::endl;
+	std::cout << typeid(f1).name() << std::endl;
+	std::cout << typeid(f2).name()<< std::endl;
+
+	c1++;
+	c2++;
+	c3++;
+	f1++;
+	/*f2++;  //  f2是整形常量，不能执行自增操作
+	*/
+	std::cout << a << " " << c1 << " " << c2 << " " << c3 << f1 << " " << f2 << " " << std::endl;
+
+	system("pause");
+	return 0;
+	
+}
