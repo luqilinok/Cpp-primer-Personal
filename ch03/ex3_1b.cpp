46eeacf72d9076cac0874f3fa2a19e55c82980ae
luqilinok <1214840350@qq.com>
2018-03-20 13:19:27 +0800

    Create ex3_1b.cpp

---
+++ ch03/ex3_1b.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//使用using声明使得cout和endl在程序中可见
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int i = 10;
+	while (i>=0)
+	{
+		cout << i << " ";
+		i--;
+	}
+	cout << endl;
+	
+	system("pause");
+	return 0;
+}
