334c6c1b74bede5eb15267d53652250588e6dc9d
luqilinok <1214840350@qq.com>
2018-03-23 15:12:58 +0800

    Create ex3_14.cpp

---
+++ ch03/ex3_14.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+using std::vector;
+
+int main()
+{
+	vector<int> vec;
+	for (int i;cin>>i;)
+	{
+		vec.push_back(i);
+	}
+
+	system("pause");
+	return 0;
+}
