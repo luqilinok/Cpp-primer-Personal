09af594e1094d95896815cd70601c9f742568bc2
luqilinok <1214840350@qq.com>
2018-03-29 16:28:20 +0800

    Create ex3_35.cpp

---
+++ ch03/ex3_35.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	const int size = 10;
+	int arr[size];
+	for (auto ptr = arr; ptr!=arr+size; ++ptr)
+	{
+		*ptr = 0;
+	}
+	for (auto i : arr) cout << " ";
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
