67793098b21bc4786d075965afed675f5927b868
luqilinok <1214840350@qq.com>
2018-03-29 13:58:59 +0800

    Update ex3_32a.cpp

--- ch03/ex3_32a.cpp
+++ ch03/ex3_32a.cpp
@@ -6,23 +6,26 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int main()
+int main()  //实现数组拷贝功能
 {
-	//array
-	int array1[10];
-	for (int i = 1; i < 10; ++i)
+	const int sz = 10;  // 常量sz作为数组的维度
+	int a[sz], b[sz];
+	//通过for循环为数组元素赋值
+	for (int i = 0; i < sz; ++i)
 	{
-		array1[i] = i;
+		a[i] = i;
 	}
-	int array2[10];
-	for (int i = 0; i < 10; ++i)
+	for (int j = 0; j < sz; ++j)
 	{
-		array2[i] = array1[i];
-
+		b[j] = j;
+	}
+	//通过范围for循环输出数组的全部元素
+	for (auto val:b )  //每次迭代，val会被初始化为b的下一个元素值
+	{
+		cout << val << " ";
 	}
 
-	
-	
 	system("pause");
+
 	return 0;
 }
