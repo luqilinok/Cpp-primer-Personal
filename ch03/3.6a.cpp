06db3e75a1f9675c1519a792157d9f3c8e5a771a
luqilinok <1214840350@qq.com>
2018-03-31 20:19:08 +0800

    Create 3.6a.cpp

---
+++ ch03/3.6a.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int ia[3][4];
+	int(*p)[4] = ia;
+	p = &ia[2];
+	//输出ia中每个元素的值，每个内层数组各占一行
+	//p指向含有4个整数的数组
+	for (auto p=ia;p!=ia+3;++p)
+	{
+		//q指向4个整数数组的首元素
+		for (auto q=*p;q != *p+4 ; ++q)
+		{
+			cout << *q << " ";
+		}
+		cout << endl;
+	}
+	
+
+	system("pause");
+	return 0;
+}
