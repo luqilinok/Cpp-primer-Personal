796103a4c51a7145826a9ded6b67bf86d453976d
luqilinok <1214840350@qq.com>
2018-04-25 21:46:47 +0800

    Create Chapter6.h

---
+++ ch06/Chapter6.h
@@ -0,0 +1,8 @@
+#ifndef CHAPTER6_H_INCLUDED
+#define CHAPTER6_H_INCLUDED
+
+
+int fact(int);  //练习6.3、6.4、练习6.5用到的函数(因为和答案所做的6.5不一样，所以用的函数也不一样，答案中double函数是ex6_5所用)
+
+#endif // CHAPTER_H_INCLUEDED
+
