817cacdd6cf4d981c55777a7c3d62734a256dfd8
luqilinok <1214840350@qq.com>
2018-04-27 13:36:08 +0800

    Create ex6_13.cpp

---
+++ ch06/ex6_13.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void a(int);   //传值参数
+void b(int&);  //传引用参数
+
+int main()
+{
+	int s = 0, t = 10;
+	a(s);
+	cout << s << endl;
+	b(t);
+	cout << t << endl;
+
+	system("pause");
+	return 0;
+}
+
+void a(int i)
+{
+	++i;
+	cout << i << endl;
+}
+void b(int &j)
+{
+	++j;
+	cout << j << endl;
+}
