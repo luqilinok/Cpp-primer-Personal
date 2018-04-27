3051c757fd43bca3e70b624d5ec51d75e898489e
luqilinok <1214840350@qq.com>
2018-04-27 13:44:51 +0800

    Update ex6_13.cpp

--- ch06/ex6_13.cpp
+++ ch06/ex6_13.cpp
@@ -1,28 +1,27 @@
 #include<iostream>
 using namespace std;
 
-void a(int);   //传值参数
-void b(int&);  //传引用参数
+void a(int i)
+{
+	++i;
+	cout << i << endl;  //当将s=0赋值给i时，输出1
+}
+void b(int &j)
+{
+	++j;
+	cout << j << endl;  //当将t=10赋值给j时，输出11
+}
+
 
 int main()
 {
 	int s = 0, t = 10;
-	a(s);
+	a(s);  // 输出0
 	cout << s << endl;
 	b(t);
-	cout << t << endl;
+	cout << t << endl;  //输出11
 
 	system("pause");
 	return 0;
 }
 
-void a(int i)
-{
-	++i;
-	cout << i << endl;
-}
-void b(int &j)
-{
-	++j;
-	cout << j << endl;
-}
