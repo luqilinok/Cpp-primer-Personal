d363c9f9c8da05694eaa7c31df773b785d90b5c2
luqilinok <1214840350@qq.com>
2018-04-29 19:20:58 +0800

    Update ex6_17.cpp

--- ch06/ex6_17.cpp
+++ ch06/ex6_17.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using std::string;
 
-bool HasUpper(const string& str)
+bool HasUpper(const string& str)   //因为判断string对象中是否含有大写字母，无需修改形参的值，因此将其设为常量引用类型
 {
 	for (auto c : str)
 	{
@@ -20,7 +20,7 @@ bool HasUpper(const string& str)
 	
 }
 
-void ChangeToLower(string& str)
+void ChangeToLower(string& str)  //该函数需要修改形参的内容，所以要将其设定为非常量引用类型
 {
 	for (auto &c:str)   //如果想改变string对象中字符的值，必须把循环变量定义成引用类型
 	{
