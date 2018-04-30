cd09659b8c10cd731cbae37d58bbf2ad6c62450c
luqilinok <1214840350@qq.com>
2018-04-30 17:10:43 +0800

    Create ex6_25.cpp

---
+++ ch06/ex6_25.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string str;
+	for (int i = 1; i!= argc; ++i)
+	{
+		str += argv[i];  //+=运算符等价于str=str+argv[i]
+	}
+
+	cout << str << endl;
+
+	system("pause");
+	return 0;
+}
