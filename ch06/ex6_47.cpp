434e502bbf96658271627898f94a045639c6a97f
luqilinok <1214840350@qq.com>
2018-05-03 21:34:24 +0800

    Create ex6_47.cpp

---
+++ ch06/ex6_47.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+using std::vector;
+
+void print(vector<int> vInt, int index)
+{
+	unsigned sz = vInt.size();
+#ifndef NDEBUG
+	cout << "vector size:" << sz << endl;
+#endif // !NDEBUG
+
+
+	if (!vInt.empty() && index < sz)
+	{
+		cout << vInt[index] << endl;
+		print(vInt, index + 1);   //递归调用
+	}
+}
+
+int main()
+{
+	vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7 };
+	print(v, 0);
+
+	system("pause");
+	return 0;
+}
