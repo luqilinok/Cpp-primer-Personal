fb9b8c79d09a3610fa3ea2eb5456b110eba8ef57
luqilinok <1214840350@qq.com>
2018-05-03 22:44:16 +0800

    Create ex6_51.cpp

---
+++ ch06/ex6_51.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void f()
+{
+	cout << "f()" << endl;
+}
+void f(int)
+{
+	cout << "f(int)" << endl;
+}
+void f(int,int)
+{
+	cout << "f(int,int)" << endl;
+}
+void f(double, double)
+{
+	cout << "f(double,double)" << endl;
+}
+
+int main()
+{
+	//f(2.56,42);// 'f' is ambiguous
+	f(42);
+	f(42, 0);
+	f(2.56, 3.14);
+
+	system("pause");
+	return 0;
+}
