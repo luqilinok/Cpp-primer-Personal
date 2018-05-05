c70fd94fbeeddf41d0d363c9464d07117cfde563
luqilinok <1214840350@qq.com>
2018-05-05 22:10:27 +0800

    Update Sales_data.h

--- ch02/Sales_data.h
+++ ch02/Sales_data.h
@@ -1,6 +1,15 @@
-struct Sales_data{
-	std::string bookNo;  //书籍编号
-	unsigned units_sold =0;  //销售量
-	double sellingprice =0.0;  //实售价
-	double discount=0.0;  //折扣
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+struct Sale_data
+{
+	std::string bookNo;      //书籍编号
+	std::string bookName;    //书籍名称
+	unsigned units_sold = 0; //销售量
+	double revenue = 0.0;    //收入
+	double price = 0.0;      //价格
+	//...
 };
