0dc304c91ac0131d46040231b5778a8e0a9a6785
luqilinok <1214840350@qq.com>
2018-05-10 09:06:09 +0800

    Update ex7_11.h

--- ch07/ex7_11.h
+++ ch07/ex7_11.h
@@ -1,46 +1,48 @@
-#include <string>
-#include <iostream>
+#include<iostream>
+#include<string>
 
-struct Sales_data {
-	Sales_data() = default;
-	Sales_data(const std::string &s) :bookNo(s) { }
-	Sales_data(const std::string &s, unsigned n, double p) :bookNo(s), units_sold(n), revenue(n*p) { }
-	Sales_data(std::istream &is);
+using namespace std;
+
+struct Sales_data
+{
+	//新增的构造函数
+	Sales_data() = default;  //一个空参列表，定义了一个默认的构造函数
+	Sales_data(const std::string &s) :bookNo(s) { }  //一个const string&，表示ISBN编号；编译器将赋予其他成员默认值
+	Sales_data(const std::string &s,unsigned n,double p):  //以下两行：一个const string&，表示ISBN编号，一个unsigned，表示售出的图书数量，一个double，表示图书的售出价格
+		bookNo(s),units_sold(n),revenue(p*n) { } //花括号之前的部分叫做构造函数初始值列表，作用是为新创建的对象的一个或几个成员函数赋值
+	
+	Sales_data(std::istream &is);   //一个istream&，从中读取一条交易信息
+
 
 	std::string isbn() const { return bookNo; };
-	Sales_data& combine(const Sales_data&);
+		Sales_data& combine(const Sales_data&);
+	double avg_price() const;
 
-	std::string bookNo;
-	unsigned units_sold = 0;
-	double revenue = 0.0;
+	std::string bookNo; //书籍编号
+	std::string bookName; //书籍名称
+	unsigned units_sold = 0; //销售量
+	double revenue = 0.0; //收入
+	double price = 0.0;  //价格
 };
 
-// nonmember functions
-std::istream &read(std::istream &is, Sales_data &item)
+
+double Sales_data::avg_price() const
 {
-	double price = 0;
-	is >> item.bookNo >> item.units_sold >> price;
-	item.revenue = price * item.units_sold;
-	return is;
+	if (units_sold)
+	{
+		return revenue / units_sold;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
-std::ostream &print(std::ostream &os, const Sales_data &item)
-{
-	os << item.isbn() << " " << item.units_sold << " " << item.revenue;
-	return os;
-}
 
-Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
-{
-	Sales_data sum = lhs;
-	sum.combine(rhs);
-	return sum;
-}
 
-// member functions.
-Sales_data::Sales_data(std::istream &is)
+inline Sales_data::Sales_data(std::istream &is)
 {
-	read(is, *this);
+	read(is, *this);  //read函数的作用是从is中读取一条交易信息，然后存入this对象中
 }
 
 Sales_data& Sales_data::combine(const Sales_data& rhs)
@@ -50,4 +52,16 @@ Sales_data& Sales_data::combine(const Sales_data& rhs)
 	return *this;
 }
 
+istream &read(istream &is, Sales_data &item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
 
+ostream &print(ostream &os, const Sales_data &item)
+{
+	os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price();
+	return os;
+}
