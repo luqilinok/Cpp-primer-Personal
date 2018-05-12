a7f0905fcfc23b4062b369d516785031f7449ada
luqilinok <1214840350@qq.com>
2018-05-12 21:27:21 +0800

    Update ex7_32b.h

--- ch07/ex7_32b.h
+++ ch07/ex7_32b.h
@@ -24,7 +24,7 @@ public:
 	Screen(pos ht,pos wd):height(ht),width(wd),contents(ht*wd,' '){ }  //2.该构造函数接受宽和高的值，然后将contents初始化成给定数量的空白
 	Screen(pos ht,pos wd,char c):height(ht),width(wd),contents(ht*wd,c){ }  //3.该构造函数接受宽和高的值以及一个字符，该字符作为初始化之后屏幕的内容
 
-	char get() const { return contents[cursor]; }  //读取光标处的字符
+	char get() const { return contents[cursor]; }
 	char get(pos r, pos c) const { return contents[r*width + c]; }
 	inline Screen& move(pos r, pos c);
 	inline Screen& set(char c);
@@ -56,13 +56,13 @@ private:
 
 inline Screen& Screen::move(pos r, pos c)
 {
-	cursor = r * width + c;
-	return *this;
+	cursor = r * width + c;   //计算行的位置，并在行内将光标移动到指定的列
+	return *this;            //以左值的形式返回对象
 }
 inline Screen& Screen::set(char c)
 {
-	contents[cursor] = c;
-	return *this;
+	contents[cursor] = c;       //计算行的位置
+	return *this;               //返回给定列的字符
 }
 inline Screen& Screen::set(pos r, pos c, char ch)
 {
