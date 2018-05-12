9ee12bf0aa439844c5fe0d494883a97dcd2c2666
luqilinok <1214840350@qq.com>
2018-05-12 15:54:43 +0800

    Create ex7_26(显式内联).h

---
+++ ch07/ex7_26(显式内联).h
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+class Sales_data
+{
+public:
+	double avg_price() const;
+
+private:
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+inline double average_price() const
+{
+	if (units_sold)
+	{
+		return revenue / units_sold;
+	}
+	else
+	{
+		return 0;
+	}
+}
