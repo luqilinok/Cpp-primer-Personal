e2178c91f97fcf89359acc60a6775e735439ee6c
luqilinok <1214840350@qq.com>
2018-05-12 15:52:11 +0800

    Create ex7_26(隐式内联).h

---
+++ ch07/ex7_26(隐式内联).h
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+class Sales_data
+{
+public:
+	double average_price() const
+	{
+		if (units_sold)
+		{
+			return revenue / units_sold;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+private:
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
