5e6959e0acff45e801336faa13068095e1b77957
luqilinok <1214840350@qq.com>
2018-05-12 21:27:43 +0800

    Update ex7_32a.h

--- ch07/ex7_32a.h
+++ ch07/ex7_32a.h
@@ -39,14 +39,14 @@ private:
 inline
 Screen &Screen::move(pos r, pos c) 
 {
-	pos row = r * width;
-	cursor = row + c;
-	return *this;
+	pos row = r * width;   //计算行的位置
+	cursor = row + c;      //在行内将光标移动到指定的列
+	return *this;          //以左值的形式返回对象
 }
 char Screen::get(pos r, pos c) const
 {
-	pos row = r * width;
-	return contents[row + c];
+	pos row = r * width;       //计算行的位置
+	return contents[row + c];  //返回给定列的字符
 }
 
 inline Screen &Screen::set(char c)
