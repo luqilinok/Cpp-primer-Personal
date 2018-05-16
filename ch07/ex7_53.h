f547ac5a43eff0dc3ffb20e0fa1f8bc8d883ed12
luqilinok <1214840350@qq.com>
2018-05-16 15:24:18 +0800

    Create ex7_53.h

---
+++ ch07/ex7_53.h
@@ -0,0 +1,20 @@
+#include<iostream>
+
+class Debug
+{
+public:
+	constexpr Debug(bool b=true):hw(b),io(b),other(b){ }
+	constexpr Debug(bool h, bool i, bool o) :
+		hw(h), io(i), other(o) { }
+	constexpr bool any() { return hw || io || other; }
+	void set_io(bool b) { io = b; }
+	void set_hw(bool b) { hw = b; }
+	void set_other(bool b) { hw = b; }
+
+private:
+	bool hw;  //硬件错误，而非IO错误
+	bool io;  //IO错误
+	bool other;   //其他错误
+};
+
+
