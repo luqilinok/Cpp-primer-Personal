75972d137412c455890871fadcbd1446ca804f8e
luqilinok <1214840350@qq.com>
2018-05-19 14:29:22 +0800

    Create ex8_10.cpp

---
+++ ch08/ex8_10.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	ifstream in("data");   //打开文件
+	if (!in)
+	{
+		cerr << "无法打开输入文件" << endl;
+		return -1;
+	}
+
+	string line;
+	vector<string> words;
+	while (getline(in,line))   //从文件中读取一行
+	{
+		words.push_back(line);  //添加到vector中
+	}
+	
+	in.close();  //输入完毕，关闭文件
+
+	vector<string>::const_iterator it = words.begin();    //先来他个迭代器
+	while (it!=words.end())   //遍历vector
+	{
+		istringstream line_str(*it);   
+		string word;
+		while (line_str>>word)    //通过istringstream从vector中读取数据
+		{
+			cout << word << " ";
+		}
+		cout << endl;
+		++it;
+	}
+
+	system("pause");
+	return 0;
+}
