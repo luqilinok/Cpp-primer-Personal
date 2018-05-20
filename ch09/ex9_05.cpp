f52e6966df5c377cf8cf1bc8497c3ba937c732b8
luqilinok <1214840350@qq.com>
2018-05-20 17:57:12 +0800

    Create ex9_05.cpp

---
+++ ch09/ex9_05.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+bool search_vec(vector<int>::iterator beg, vector<int>::iterator end, int val)
+{
+	for (; beg < end; beg++)
+	{
+		if (*beg==val)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+int main()
+{
+	vector<int> ilist = { 1,2,3,4,5,6,7 };
+
+	cout << search_vec(ilist.begin(), ilist.end(), 3) << endl;
+	cout << search_vec(ilist.begin(), ilist.end(), 8) << endl;
+
+	system("pause");
+	return 0;
+}
