b535da286d16faa0b9809295f205d771a7403b66
luqilinok <1214840350@qq.com>
2018-05-30 16:12:35 +0800

    Create ex10_07_b.cpp

---
+++ ch10/ex10_07_b.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+	vector<int> vec;
+	vec.reserve(10);
+	fill_n(back_inserter(vec), 10, 0);
+
+	for (auto iter = vec.begin(); iter !=vec.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
