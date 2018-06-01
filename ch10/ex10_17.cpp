8e8325508709e53c47d39818df141ea6163f27b3
luqilinok <1214840350@qq.com>
2018-06-01 13:16:45 +0800

    Create ex10_17.cpp

---
+++ ch10/ex10_17.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<algorithm>
+#include"Sales_data(class).h"
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	ifstream in(argv[1]);
+	if (!in)
+	{
+		cout << "打开输入文件失败！" << endl;
+		return -1;
+	}
+
+	vector<Sales_data> sds;
+	Sales_data sd;
+	while (read(in, sd))
+	{
+		sds.push_back(sd);
+	}
+
+	sort(sds.begin(), sds.end(), [](const Sales_data &lhs, const Sales_data &rhs) {return lhs.isbn() < rhs.isbn(); });
+
+	for (const auto &s : sds)
+	{
+		print(cout, s);
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
