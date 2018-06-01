3712a7ddd45847cd912f0d54b46edd592bf08613
luqilinok <1214840350@qq.com>
2018-06-01 11:31:57 +0800

    Create ex10_15.cpp

---
+++ ch10/ex10_15.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+void add(int a)
+{
+	auto sum = [a](int b) {return a + b; };
+
+	cout << sum(1) << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	add(1);
+	add(2);
+
+	system("pause");
+	return 0;
+}
