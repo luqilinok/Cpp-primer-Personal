db1c8c1a229fc9db479714081c290f84cef038f3
luqilinok <1214840350@qq.com>
2018-06-03 14:47:17 +0800

    Create ex10_27.cpp

---
+++ ch10/ex10_27.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	vector<int> vi = { 1,2,2,3,4,5,5,6 };
+	list<int> li;
+
+	unique_copy(vi.begin(), vi.end(), back_inserter(li));
+
+	for (auto c:li)
+	{
+		cout << c << " ";
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
