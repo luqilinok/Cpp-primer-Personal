4caecd9645ebcb6048583a9f0723910560063f45
luqilinok <1214840350@qq.com>
2018-06-17 15:19:06 +0800

    Update ex12_33.h

--- ch12/ex12_33.h
+++ ch12/ex12_33.h
@@ -28,9 +28,7 @@ TextQuery::TextQuery(std::ifstream &ifs)
 		file.push_back(line);
 		std::istringstream line_stream(line);
 		for (string text, word; line_stream >> text; word.clear()) {
-			// avoid read a word followed by punctuation(such as: word, )
 			std::remove_copy_if(text.begin(), text.end(), std::back_inserter(word), ispunct);
-			// use reference avoid count of shared_ptr add.
 			auto &nos = result[word];
 			if (!nos) nos.reset(new std::set<StrBlob::size_type>);
 			nos->insert(lineNo);
@@ -39,7 +37,6 @@ TextQuery::TextQuery(std::ifstream &ifs)
 }
 QueryResult TextQuery::query(const string& str) const
 {
-	// use static just allocate once.
 	static shared_ptr<std::set<StrBlob::size_type>> nodate(new std::set<StrBlob::size_type>);
 	auto found = result.find(str);
 	if (found == result.end()) return QueryResult(str, nodate, file);
@@ -51,6 +48,7 @@ public:
 	friend std::ostream& print(std::ostream &, const QueryResult&);
 public:
 	QueryResult(const string &s, shared_ptr<std::set<StrBlob::size_type>> set, const StrBlob& f) : word(s), nos(set), file(f) { }
+	//下面三行是新增加的功能
 	ResultIter begin() const { return nos->begin(); }
 	ResultIter end() const { return nos->end(); }
 	shared_ptr<StrBlob> get_file() const { return std::make_shared<StrBlob>(file); }
