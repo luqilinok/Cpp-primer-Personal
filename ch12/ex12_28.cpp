24a8eac2ee032515317eb0d5fe7431edfa3ff74e
luqilinok <1214840350@qq.com>
2018-06-17 10:39:08 +0800

    Update ex12_28.cpp

--- ch12/ex12_28.cpp
+++ ch12/ex12_28.cpp
@@ -1,24 +1,44 @@
-#include<iostream>
-#include"demo.h"
+#include <string>
+using std::string;
 
-using namespace std;
+#include <vector>
+using std::vector;
 
-void runQueries(ifstream &infile)
-{
-	TextQuery tq(infile);
-	while (true)
-	{
-		cout << "enter word  to look for,or q to quit:";
-		string s;
-		if ((cin>>s)||s=="q")
-		{
-			break;
-		}
-		print(cout, tq.query(s))<<endl;
-	}
-}
+#include <memory>
+using std::shared_ptr;
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <algorithm>
+
 int main()
 {
-	std::ifstream file("../data/storyDataFile.txt");
-	runQueries(file);
+    std::ifstream file("../data/letter.txt");
+    vector<string> input;
+    std::map<string, std::set<decltype(input.size())>> dictionary;
+    decltype(input.size()) lineNo{ 0 };
+    
+    for (string line; std::getline(file, line); ++lineNo) {
+        input.push_back(line);
+        std::istringstream line_stream(line);
+        for (string text, word; line_stream >> text; word.clear()) {
+            std::remove_copy_if(text.begin(), text.end(), std::back_inserter(word), ispunct);
+            dictionary[word].insert(lineNo);
+        }
+    }
+    
+    while (true) {
+        std::cout << "enter word to look for, or q to quit: ";
+        string s;
+        if (!(std::cin >> s) || s == "q") break;
+        auto found = dictionary.find(s);
+        if (found != dictionary.end()) {
+            std::cout << s << " occurs " << found->second.size() << (found->second.size() > 1 ? " times" : " time") << std::endl;
+            for (auto i : found->second)
+                std::cout << "\t(line " << i+1 << ") " << input.at(i) << std::endl;
+        } else std::cout << s << " occurs 0 time" << std::endl;
+    }
 }
