0a2ae2c95d58b2938d56cbb4147d20d318f23262
luqilinok <1214840350@qq.com>
2018-06-29 14:21:24 +0800

    Create ex14_21.h

---
+++ ch14/ex14_21.h
@@ -0,0 +1,70 @@
+#include <string>
+#include <iostream>
+
+class Sales_data {
+	friend std::istream& operator>>(std::istream&, Sales_data &);//输入
+	friend std::ostream& operator<<(std::ostream&, const Sales_data &);//输出
+	friend Sales_data operator+(const Sales_data &, const Sales_data &);
+
+public:
+	Sales_data() = default;
+	Sales_data(const std::string &s) :bookNo(s) { }
+	Sales_data(const std::string &s, unsigned n, double p) :bookNo(s), units_sold(n), revenue(n*p) { }
+	Sales_data(std::istream &is);
+
+	std::string isbn() const { return bookNo; };
+	Sales_data& combine(const Sales_data&);
+	Sales_data& operator+=(const Sales_data &rhs);
+private:
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+	double avg_price() const;
+};
+
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+
+inline std::istream & operator>>(std::istream &is, Sales_data &item)
+{
+	double price = 0.0;
+	is >> item.bookNo >> item.units_sold >> price;
+	if (is)
+	{
+		item.revenue = price * item.units_sold;
+	}
+	else
+	{
+		item = Sales_data();
+	}
+	return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const Sales_data &item)
+{
+	os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+inline Sales_data operator+(const Sales_data &lhs, const Sales_data &rhs)
+{
+	Sales_data sum = lhs;
+	sum += rhs;
+	return sum;
+}
+
+Sales_data& Sales_data::operator+=(const Sales_data &rhs)
+{
+	units_sold += rhs.units_sold;
+	revenue = rhs.revenue;
+	return *this;
+}
+
+inline double Sales_data::avg_price() const
+{
+	return units_sold ? revenue / units_sold : 0;
+}
