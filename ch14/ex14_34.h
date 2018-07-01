b8ab9a91dd74a22c79671ce1eeba7c2a5156312e
luqilinok <1214840350@qq.com>
2018-07-01 18:58:55 +0800

    Create ex14_34.h

---
+++ ch14/ex14_34.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+class IFElseThen
+{
+public:
+	IFElseThen() {}
+	IFElseThen(int i1, int i2, int i3) :iVal1(i1), iVal2(i2), iVal3(i3) {}
+
+	int operator()(int i1, int i2, int i3)
+	{
+		return i1 ? i2 : i3;
+	}
+
+private:
+	int iVal1, iVal2, iVal3;
+};
