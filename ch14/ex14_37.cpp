efd0a9e9bb24e11df7b15cb5556a20a30214e44c
luqilinok <1214840350@qq.com>
2018-07-02 09:12:06 +0800

    Update ex14_37.cpp

--- ch14/ex14_37.cpp
+++ ch14/ex14_37.cpp
@@ -16,11 +16,11 @@ private:
 
 int main()
 {
-	vector<string> vec = { 1,2,3,2,1 };
+	vector<int> vec = { 1,2,3,2,1 };
 	const int oldValue = 2;
 	const int newValue = 200;
 	IntCompare icmp(oldValue);
 	std::replace_if(vec.begin(), vec.end(), icmp, newValue);  //replace()将区间[beg,end)之内每一个“与oldValue相等”的元素替换为newValue
-
+	system("pause");
 	return 0;
 }
