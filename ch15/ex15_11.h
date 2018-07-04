b33893ba6d213f0df9cd5ea8aa24acb5922eb9c0
luqilinok <1214840350@qq.com>
2018-07-04 15:07:27 +0800

    Create ex15_11.h

---
+++ ch15/ex15_11.h
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Quote
+{
+public:
+	Quote() = default;
+	Quote(const std::string &book, double sales_price) :bookNo(book), price(sales_price) {}
+	std::string isbn() const { return bookNo; }   //返回给定数量的书籍的销售总额，派生类改写并使用不同的折扣计算方法
+	virtual double net_price(std::size_t n)const { return n * price; }
+	virtual void debug()
+	{
+		cout << "bookNo=" << bookNo << "price=" << price << endl;
+	}
+	virtual ~Quote() = default;   //对析构函数进行动态绑定
+private:
+	std::string bookNo;  //书籍的ISBN编号
+protected:
+	double price = 0.0;  //代表普通状态下不打折的价格
+};
+
+double print_total(ostream &os, const Quote &item, size_t n)
+{
+	//根据传入item形参的对象类型调用Quote::net_price
+	//或者BUlk_quote::net_price
+	double ret = item.net_price(n);
+	os << "ISBN:" << item.isbn() << "# sold:" << n << "total due: " << ret << endl;
+
+	return ret;
+}
+
+class Bulk_quote :public Quote
+{
+public:
+	Bulk_quote(const std::string &book, double p, std::size_t qty, double disc) :Quote(book, p), max_qty(qty), discount(disc) {}
+	double net_price(size_t cnt) const override
+	{
+		if (cnt <= min_qty)
+		{
+			return cnt * (1 - discount)*price;
+		}
+		else
+		{
+			return cnt * price;
+		}
+	}
+	virtual void debug()
+	{
+		cout << "min_qty=" << min_qty << " discount=" << discount << endl;  //bookNo变量为private，所以不能直接访问bookNO,只能调用基类的debug()函数来显式
+	}
+private:
+	size_t min_qty;
+	double discount;
+};
