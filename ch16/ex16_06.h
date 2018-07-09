50bb993a33be2553dca6109f387f68736c95c3a9
luqilinok <1214840350@qq.com>
2018-07-09 14:49:00 +0800

    Create ex16_06.h

---
+++ ch16/ex16_06.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+template<typename T,size_t N>
+
+const T *my_begin(const T(&a)[N])
+{
+	return &a[0];
+}
+
+template<typename T,size_t N>
+
+const T *my_end(const T(&a)[N])
+{
+	return &a[0] + N;
+}
