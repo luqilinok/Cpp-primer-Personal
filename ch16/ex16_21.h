ed7026855750585f7cb0e76d60a00486ae9306d4
luqilinok <1214840350@qq.com>
2018-07-10 22:19:20 +0800

    Create ex16_21.h

---
+++ ch16/ex16_21.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class DebugDelete
+{
+public:
+	DebugDelete(ostream &s = cerr) :os(s) {}
+	template<typename T> void operator()(T *p) const { os << "deleting unique_ptr" << endl; delete p; }
+
+private:
+	ostream & os;
+};
