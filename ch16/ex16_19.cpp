12f6bea0535562949feab164aa15e042dc9f05aa
luqilinok <1214840350@qq.com>
2018-07-10 21:50:37 +0800

    Create ex16_19.cpp

---
+++ ch16/ex16_19.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+template<typename C>
+
+void print(const C &c)
+{
+	for (typename C::size_type i = 0; i < c.size(); ++i)
+	{
+		cout << c.at(i) << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	string s = "hello";
+	print(s);
+
+	vector<int> vi = { 0,2,4,6,8 };
+	print(vi);
+
+	system("pause");
+	return 0;
+}
