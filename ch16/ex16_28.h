9f2d1d44c9a163808607e8d9af049f707dd1a1fc
luqilinok <1214840350@qq.com>
2018-07-11 15:28:22 +0800

    Update ex16_28.h

--- ch16/ex16_28.h
+++ ch16/ex16_28.h
@@ -1,82 +1,91 @@
 #ifndef SP_H
 #define SP_H
 
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-template<typename T>
-
+template <typename T>
 class SP
 {
 public:
-	SP() :p(nullptr), use(nullptr) {}
-	explicit SP(T *pt) :P(pt), use(new size_t(l)) {}
-	SP(const SP &sp) :p(sp.p), use(sp.use) { if (use)++*use; }  //拷贝构造函数
-	SP& operator=(const SP&);  //拷贝赋值运算符
+	SP() : p(nullptr), use(nullptr) {}
+	explicit SP(T* pt) : p(pt), use(new size_t(1)) {}
+	SP(const SP &sp) :
+		p(sp.p), use(sp.use) {
+		if (use)++*use;
+	}
+	SP& operator=(const SP&);
 	~SP();
-	T& operator*() { return *p; } //解引用
-	T& operator*() const { return *p; }  //const版本的解引用
+	T& operator*() { return *p; }
+	T& operator*() const { return *p; }
 
 private:
 	T * p;
 	size_t *use;
 };
 
-template<typename T>
-SP<T>&SP<T>::operator=(const SP<T> &rhs)
+template <typename T>
+SP<T>::~SP()
+{
+	if (use && --*use == 0)
+	{
+		delete p;
+		delete use;
+	}
+}
+
+template <typename T>
+SP<T>& SP<T>::operator=(const SP<T> &rhs)
 {
 	if (rhs.use)
 	{
-		++*rhs.use;   //递增右侧运算对象的引用计数
+		++*rhs.use;
 	}
-
-	if (use&&--*use == 0)   //然后递减本对象的引用计数
+	if (use && --*use == 0)
 	{
-		delete  p;   //如果没有其他用户
-		delete use;  //释放本对象分配的成员
+		delete p;
+		delete use;
 	}
 	p = rhs.p;
-	use = rhs.ues;
-	return *this;   //返回本对象
+	use = rhs.use;
+	return *this;
 }
 
-template<typename T, class...Args>
-SP<T> make_SP(Args&&...args)
+template <typename T, class... Args>
+SP<T> make_SP(Args&&... args)
 {
 	return SP<T>(new T(std::forward<Args>(args)...));
 }
 
-template<typename T>
-
+template <typename T>
 class UP
 {
 public:
-	UP() :p(nullptr) {}
-	UP(const UP&) = delete;  //禁止拷贝构造函数
-	explicit UP(T *pt) :p(pt) {}   //构造函数
-	UP& operator=(const UP&) = delete;  //禁止拷贝赋值运算符
+	UP() : p(nullptr) {}
+	UP(const UP &) = delete;
+	explicit UP(T* pt) : p(pt) {}
+	UP& operator=(const UP&) = delete;
 	~UP();
-	T *release();    //交出控制权
-	void reset(T *new_p);   //释放对象
-	T& operator*() { return *p; }  //解引用运算符
-	T& operator*()const { return *p; }  //const版本的解引用运算符
+	T* release();
+	void reset(T *new_p = nullptr);
+	T& operator*() { return *p; }
+	T& operator*() const { return *p; }
 
 private:
 	T * p;
 };
 
-template<typename T>
+template <typename T>
 UP<T>::~UP()
 {
-	if (p)   //如果已经分配了内存空间
+	if (p)
 	{
-		delete p;   //释放对象内存
+		delete p;
 	}
 }
 
-template<typename T>
-void UP<T>::reset(T *new_p = nullptr)
+template <typename T>
+void UP<T>::reset(T *new_p )
 {
 	if (p)
 	{
@@ -85,12 +94,12 @@ void UP<T>::reset(T *new_p = nullptr)
 	p = new_p;
 }
 
-template<typename T>
+template <typename T>
 T* UP<T>::release()
 {
 	T *q = p;
 	p = nullptr;
-
 	return q;
 }
-#endif // !SP_H
+
+#endif
