3de60737b9b6080fccb7b070a675bb696ace7236
luqilinok <1214840350@qq.com>
2018-07-11 15:48:29 +0800

    Create ex16_30.cpp

---
+++ ch16/ex16_30.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include"ex16_29_sp_Blob.h"
+
+using namespace std;
+
+int main()
+{
+	Blob<string> b1;
+	cout << b1.size() << endl;
+	{
+		Blob<string> b2 = { "a","an","the" };
+		b1 = b2;
+		b2.push_back("about");
+		cout << b1.size() << " " << b2.size() << endl;
+	}
+	cout << b1.size() << endl;
+	for (size_t i = 0; i < b1.size(); ++i)
+	{
+		cout << b1.at(i) << " ";
+	}
+	cout << endl << endl;
+
+	UP<int> u1(new int(42));
+	cout << *u1 << endl;
+	UP<int> u2(u1.release());
+	cout << *u2 << endl;
+
+	system("pause");
+	return 0;
+}
