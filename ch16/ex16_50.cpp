dba24c0650c59f99ba2c3dcf22dcfeb60fd4a5db
luqilinok <1214840350@qq.com>
2018-07-13 09:17:06 +0800

    Create ex16_50.cpp

---
+++ ch16/ex16_50.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<typeinfo>
+
+using namespace std;
+
+template<typename T> void f(T a)
+{
+	cout << "f(T),T是" << typeid(T).name() << endl;
+}
+
+template<typename T> void f(const T *a)
+{
+	cout << "f(const T*),T是" << typeid(const T*).name() << endl;
+}
+
+template<typename T> void g(T a)
+{
+	cout << "g(T),T是" << typeid(T).name() << endl;
+}
+
+template<typename T> void g(T *a)
+{
+	cout << "g(T*),T*是"typeid(T*).name() << endl;
+}
+
+int main()
+{
+	int i = 42, *p = &i;
+	const int ci = 0, *p2 = &ci;
+	g(42); g(p); g(ci); g(p2);
+	f(42); f(p); f(ci); f(p2);
+
+	system("pause");
+	return 0;
+}
