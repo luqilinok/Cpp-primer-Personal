29b29dfed42ddc9daeccbaa495983b13d55b4831
luqilinok <1214840350@qq.com>
2018-07-16 16:57:26 +0800

    Create ex17_18.cpp

---
+++ ch17/ex17_18.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+#include<regex>
+
+using namespace std;
+
+int main()
+{
+	//查找不在字符c之后的字符串ei
+	string pattern("[^c]ei");
+	//我们需要包含pattern的整个单词
+	pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
+	regex r(pattern, regex::icase);   //构造一个用于查找模式的reget
+	smatch results;    //定义一个对象保存搜索结果
+	//定义一个string保存于模式匹配不匹配的文本
+	string line;
+	while (1)
+	{
+		cout << "Enter a word,or q to quit:";
+		getline(cin, line);
+		if (line == "q")
+		{
+			break;
+		}
+		//它将反复调用regex_search来寻找文件中的所有匹配
+		for (sregex_iterator it(line.begin(), line.end(), r), end_it; it != end_it; ++it)
+		{
+			if (it->str() != "albeit"&&it->str() != "neighbor")
+			{
+				cout << it->str() << endl;
+			}
+			else
+			{
+				cout << it->str() << endl;
+			}
+		}
+
+		system("pause");
+		return 0;
+	}
+}
