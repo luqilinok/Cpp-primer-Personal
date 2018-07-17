cfa1a3969bd1281666617ee62964908ddc8a704f
luqilinok <1214840350@qq.com>
2018-07-17 16:47:08 +0800

    Create ex17_28.cpp

---
+++ ch17/ex17_28.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<random>
+
+using namespace std;
+
+unsigned int rand_int()
+{
+	static uniform_int_distribution<unsigned> u(0, 9999);
+	static default_random_engine e;
+
+	return u(e);
+}
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << rand_int() << endl;
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
